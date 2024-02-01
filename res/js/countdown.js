f7c2d2e21aaa988fb6e1d85cea3f18226b11c5f3
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2024-02-01 18:10:47 +0100

    Project setup

---
+++ res/js/countdown.js
@@ -0,0 +1,10 @@
+$('.countdown-timer').countdown({
+    date: "October 16, 2017 10:00:00",
+
+    render: function(data) {
+        $(".days .value").html(this.leadingZeros(data.days, 2));
+        $(".hours .value").html(this.leadingZeros(data.hours, 2));
+        $(".minutes .value").html(this.leadingZeros(data.min, 2));
+        $(".seconds .value").html(this.leadingZeros(data.sec, 2));
+    }
+});
